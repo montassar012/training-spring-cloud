102cca590a12b4138ef4f45e7aaeba22e44a38ab
Montassar Kallali <montassar.kallali@wonderbox.fr>
2018-01-21 23:07:03 +0100

    Add  demo for  config client

---
+++ config_client/src/main/java/com/firststep/spring/cloud/config_client/ConfigClientApplication.java
@@ -0,0 +1,18 @@
+package com.firststep.spring.cloud.config_client;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+
+
+}
+
